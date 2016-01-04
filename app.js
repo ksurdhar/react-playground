4faf58356574f8d95a35da7205c742fced4e8ea1
Kiran Surdhar <ksurdhar1@gmail.com>
2016-01-04 11:20:19 -0700

    es6 and react babel configurations

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+import React from "react";
+import Hello from "./hello";
+
+React.render(
+  <Hello name="World" />,
+  document.body
+);
+
+let banana = 'hey';
+
+(() => { console.log(banana) })();
+
 console.log('Webpack workkdlfsjking, foolksjsadjlksajldakjsn.');
