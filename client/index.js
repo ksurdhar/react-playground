328f2f644cbdc0a8996b004a9817dbf3dfdf7e6b
Kiran Surdhar <ksurdhar1@gmail.com>
2016-01-19 16:13:08 -0700

    added brackets around Provider import

--- client/index.js
+++ client/index.js
@@ -1,28 +1,29 @@
 import React       from 'react';
 import { render }  from 'react-dom';
-// import { Router }  from 'react-router';
-// import createBrowserHistory from 'history/lib/createBrowserHistory';
-// import routes      from '../shared/routes.js';
-// import * as reducers from '../shared/reducers/index';
-// import { createStore, combineReducers } from 'redux';
-// import Provider from 'react-redux';
-// import { fromJS } from 'immutable';
+import { Router }  from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import routes      from '../shared/routes.js';
+import * as reducers from '../shared/reducers/index';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { fromJS } from 'immutable';
 
 import Hello from './Hello';
 
-// const history = createBrowserHistory();
-// const initialState = {};
-// const reducer = combineReducers(reducers);
-// const store = createStore(reducer, initialState);
-
-render(
-  <Hello name="kiran"/>,
-  document.getElementById('react-view')
-);
+const history = createBrowserHistory();
+const initialState = {};
+const reducer = combineReducers(reducers);
+const store = createStore(reducer, initialState);
 
 // render(
-//   <Provider store={ store }>
-//     <Router children={ routes } history={ history } />
-//   </Provider>,
+//   <Hello name="kiran"/>,
 //   document.getElementById('react-view')
 // );
+
+console.log('I AM THE STORE', store);
+render(
+  <Provider store={ store }>
+    <Router children={ routes } history={ history } />
+  </Provider>,
+  document.getElementById('react-view')
+);
--- shared/reducers/TodoReducer.js
+++ shared/reducers/TodoReducer.js
@@ -5,9 +5,9 @@ const defaultState = new Immutable.List();
 export default function todoReducer(state = defaultState, action) {
   switch(action.type) {
     case 'GET_TODOS':
-      return new Immutable.List(action.res.data);
+      return new Immutable.List(action.data);
     case 'CREATE_TODO':
-      return state.concat(action.res.data.text);
+      return state.concat(action.data.text);
     case 'EDIT_TODO':
       return state.set(action.id, action.text);
     case 'DELETE_TODO':
